d4f79110fe52650e70eec2107cc5c785773b6fa9
Sohamdas16 <sohamd824@gmail.com>
2025-10-26 22:04:32 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,10 @@
+num1=float(input("Enter the first number"))
+num2=float(input("Enter the second number"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Divison:",div)
